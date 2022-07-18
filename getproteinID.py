f2c3aaab5b6e5455a6eacf8bafeb98bae97c7b51
christelvanharen <cca.vanharen@student.han.nl>
2022-07-18 17:31:43 +0200

    accessiecodes verandert

--- getproteinID.py
+++ getproteinID.py
@@ -13,23 +13,28 @@ def protein(input_file, output_file):
     """
     Extracts the accession ID from the FASTA file of the chromosome.
     """
-    proteinIDs = []
-    with open(input_file, "r") as c:
-        for line in c:
-            if line.startswith(">"):
-                ids = line.split("|")
-                proteinIDs.append(ids[1])
-                write_file = open(output_file, "w")
-                write_file.write(str(proteinIDs))
-                # correct adjustments made for in file
-                with open(output_file, "r") as p:
-                    for lines in p:
-                        id = lines.replace("', '", "\n")
-                        ids = id.removeprefix("['").removesuffix("']")
-                        print(ids)
-                        write_file = open(output_file, "w")
-                        write_file.write(ids)
-                write_file.close()
+    try:
+        proteinIDs = []
+        with open(input_file, "r") as c:
+            for line in c:
+                if line.startswith(">"):
+                    ids = line.split("|")
+                    gn = ids[2].split("GN=")
+                    gen_id = gn[1].split(" ")
+                    proteinIDs.append(gen_id[0])
+                    write_file = open(output_file, "w")
+                    write_file.write(str(proteinIDs))
+                    # correct adjustments made for in file
+                    with open(output_file, "r") as p:
+                        for lines in p:
+                            id = lines.replace("', '", "\n")
+                            ids = id.removeprefix("['").removesuffix("']")
+                            print(ids)
+                            write_file = open(output_file, "w")
+                            write_file.write(ids)
+                    write_file.close()
+    except IndexError:
+        pass
 
 
 def main(input_file, output_file):
