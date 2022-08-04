1cc5a67d8308c6677414cbe2e3177a3ea7deee71
christelvanharen <cca.vanharen@student.han.nl>
2022-08-04 16:24:16 +0200

    adjusted files

--- linear_regression.py
+++ linear_regression.py
@@ -38,7 +38,7 @@ def regressieanalyse(filename_genotypes):
             info = info2.split("\t")
             info = list(map(str, info))
             genotype.append(str(info[1:])) #skips the first colums
-        print(genotype)
+        # print(genotype)
         return genotype
 
 
@@ -53,39 +53,40 @@ def analysis(genotype, count, directory):
         for k in count:
             x = np.array(i) #genotype
             y = np.array(k) #phenotype
+            print(x)
 
-            if x.__contains__(0) and x.__contains__(
-                    1) or x.__contains__(
-                0) and x.__contains__(2):
+            # if x.__contains__(0) and x.__contains__(
+            #         1) or x.__contains__(
+            #     0) and x.__contains__(2):
 
-                slope, intercept, r, p, std_err = stats.linregress(x, y)
-                # if stats.ttest_ind(x, y) <= 0.05:
-                def myfunc(x):
-                    """
-                    This calculates the coefficient and returns it.
-                    """
-                    return slope * x + intercept #y=ax+b
+            slope, intercept, r, p, std_err = stats.linregress(x, y)
+            # if stats.ttest_ind(x, y) <= 0.05:
+            def myfunc(x):
+                """
+                This calculates the coefficient and returns it.
+                """
+                return slope * x + intercept #y=ax+b
 
-                mymodel = list(map(myfunc, x))
+            mymodel = list(map(myfunc, x))
 
-                # if the counts(phenotype) have all 0's in a row,
-                # the plot will not be made.
-                if mymodel != [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]:
-                    plt.scatter(x, y)
-                    plt.plot(x, mymodel, color='black')
-                    plt.title("Regression Analysis")
-                    plt.xlabel("Genotype")
-                    plt.ylabel("Phenotype")
-                    plt.savefig(f"{directory}/eQTL{counts}.jpg")
-                    plt.clf()
-                    plt.close()
-                    counts += 1
-                else:
-                    continue
-                # else:
-                #     continue
+            # if the counts(phenotype) have all 0's in a row,
+            # the plot will not be made.
+            if mymodel != [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]:
+                plt.scatter(x, y)
+                plt.plot(x, mymodel, color='black')
+                plt.title("Regression Analysis")
+                plt.xlabel("Genotype")
+                plt.ylabel("Phenotype")
+                plt.savefig(f"{directory}/eQTL{counts}.jpg")
+                plt.clf()
+                plt.close()
+                counts += 1
             else:
                 continue
+                # else:
+                #     continue
+            # else:
+            #     continue
 
 
 def main(filename_rawcounts, filename_genotypes, directory):
