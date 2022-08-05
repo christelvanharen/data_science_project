768e1d6fc94662bd5e92a2d19c7d153f650bf2bb
christelvanharen <cca.vanharen@student.han.nl>
2022-08-05 14:13:12 +0200

    adjusted files

--- dots.py
+++ dots.py
@@ -1,4 +1,22 @@
+"""
+Author: Christel van Haren
+The normalised counts file had versions of the gene names,
+like ENSG00000260370.1, and removes the versions. So everything that
+comes after the '.' will be removed and will look like ENSG00000260370.
+Adjusted date: 4 august 2022
+"""
+import sys
+
+
 def dots(file, new_file):
+    """
+    Removes the versions of the gene names.
+    :param file: the normalised counts file with the versions of the
+    gene names
+    :param new_file: the new normalised counts file without the
+    versions of the gene names
+    :return: the new written file, with new_file in it.
+    """
     write_file = open(new_file, "w")
     with open(file, "r") as c:
         for line in c:
@@ -12,10 +30,9 @@ def dots(file, new_file):
     write_file.close()
 
 
-def main():
-    file = "genormaliseerde_waarden_nieuw.txt"
-    new_file = "final_normalised_counts.txt"
+def main(file, new_file):
     dots(file, new_file)
 
 
-main()
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
